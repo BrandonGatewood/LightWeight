7d6f12a3f3f1a89ab198581c02716b75095b3e2a
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2023-11-14 15:39:41 -0800

    Implemented server to connect to mongodb. Included split.js endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
 const express = require('express');
+
+// Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    next(); 
+});
+
+// Routes
+app.use('/api/workouts', workoutRoutes);
+
+// Connect to mongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
