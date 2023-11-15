51041603b9ff90420d03df7c9f45553bbcb418d4
Brandon Gatewood <90942107+BrandonGatewood@users.noreply.github.com>
2023-11-15 14:50:55 -0800

    Implemented controllers for each req for split routes

--- backend/controllers/splitController.js
+++ backend/controllers/splitController.js
@@ -10,21 +10,63 @@ const getSplits = async (req, res) => {
 
 // GET a single split
 const getSplit = async (req, res) => {
+    const { id } = req.params;
 
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(400).json({ error : 'Invalid id' });
+
+    const split = await Split.findById(id);
+
+    if(!split)
+        return res.status(404).json({ error : 'No Such Split found.' });
+
+    res.status(200).json(split);
 }
 
 // POST a new split 
 const postSplit = async (req, res) => {
+    const { name, numOnDays, numOffDays, workouts } = req.body;
 
+    // Add document to database
+    try {
+        const split = await Split.create({ name, numOnDays, numOffDays, workouts });
+        res.status(200).json(split);
+    }
+    catch(error) {
+        res.status(400).json({ error : error.message });
+    }
 }
 
 // DELETE a single split 
 const deleteSplit = async (req, res) => {
+    const { id } = req.params;
 
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(400).json({ error : 'Invalid id' });
+
+    const split = await Split.findOneAndDelete(id);
+
+    if(!split)
+        return res.status(404).json({ error : 'No Such Split found.' });
+
+    res.status(200).json(split);
 }
 
 // PATCH a split 
 const patchSplit = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(400).json({ error : 'Invalid id' });
+
+    const split = await Split.findOneAndUpdate({ _id : id }, {
+        ...req.body
+    });
+
+    if(!split)
+        return res.status(404).json({ error : 'No Such Split found.' });
+
+    res.status(200).json(split);
 
 }
 
