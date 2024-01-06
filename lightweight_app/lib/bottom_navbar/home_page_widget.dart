a5ef873ed9fc57b02cc7cf90fa1b7128511e2221
Brandon Gatewood <gatew2@pdx.edu>
2024-01-05 18:33:06 -0800

    removing unnecessary code

--- lightweight_app/lib/bottom_navbar/home_page_widget.dart
+++ lightweight_app/lib/bottom_navbar/home_page_widget.dart
@@ -11,13 +11,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Open a connection to DB
-
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(left: 15),
         child: ListView(
-          // Todays Workout 
           children: const <Widget>[
             Text('Summary'),
             Summary(),
@@ -27,115 +24,13 @@ class HomePage extends StatelessWidget {
             Plan(),
             Text('Highlights'),
             Highlight(),
-            // Planning
-           
-
           ],
         ),
       ),
-      persistentFooterButtons: <Widget>[
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const EditWorkoutsPage())); 
-          },
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: const Text('Edit Split',
-            style: TextStyle(
-              color: Colors.white,
-            ), ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TrackWorkoutPage()));
-          },
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            backgroundColor: Colors.blue,
-          ),
-          child: const Wrap(
-            children: <Widget>[
-              Icon(Icons.track_changes_rounded,
-                color: Colors.white,
-              ),
-              Text('Track Workout',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ]
-          ),
-        ),
-      ],
-      persistentFooterAlignment: AlignmentDirectional.center,
     );
   }
 }
 
-   /* 
-    return Column(
-      children: <Widget>[
-        SizedBox(
-          // SizedBox Styling 
-          height: 500,
-          child: ListView(
-            children: const <Widget>[
-              Card(
-                child: ListTile(
-                  title: Text('Incline Dumbbell Bench'),
-                  subtitle: Text('\t3 x 10'),
-                ),
-              ),
-            ],
-          ),
-        ),
-        // Edit Workouts Button and Track Workouts Button
-        Expanded(
-          child: Row(
-            children: <Widget>[
-              const Spacer(
-
-              ),
-              ElevatedButton(
-                child: const Text('Edit Workouts'),
-                onPressed: () {
-                  
-                },
-              ),
-              const Spacer(
-              ),
-              ElevatedButton(
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll<Color>(Colors.lightBlue),
-                ),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const TrackWorkoutPage()));
-                },
-                child: const Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.track_changes_rounded
-                    ),
-                    Spacer(
-
-                    ),
-                    Text("Track Workout"), // text
-                  ],
-                ),
-              ),
-              const Spacer(
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  */ 
   /*
     findDay function uses DateTime class to find the current day.
 
