22ca4f0365afeb8d91cf49de6c365c5c02275e86
Brandon Gatewood <gatew2@pdx.edu>
2024-01-09 09:46:42 -0800

    Implmented highlights widget with health chart and a scrollable chart.

--- lightweight_app/lib/bottom_navbar/home_page_widgets/highlight_widget.dart
+++ lightweight_app/lib/bottom_navbar/home_page_widgets/highlight_widget.dart
@@ -1,78 +1,123 @@
 import "package:flutter/material.dart";
-
-class Highlight extends StatelessWidget {
+import 'package:fl_chart/fl_chart.dart';
+class Highlight extends StatefulWidget {
   const Highlight({super.key});
 
   @override
+  State<Highlight> createState() => _Highlight(); 
+}
+
+class _Highlight extends State<Highlight> {
+  late int selectedPage;
+  late final PageController _pageController;
+
+  @override
+  void initState() {
+    selectedPage = 0;
+    _pageController = PageController(initialPage: selectedPage);
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-   return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    int pageCount = 4;
+    return SizedBox(
+      height: 500,
+      child: Column(
+        children: <Widget>[
+          weightHighlight(context),
+          //exerciseHighlight(),
+          SizedBox(
+            height: 200,
+            child: favoriteExercise(context),
+          ),
+
+ 
+
+
+
+        ],
+      )
+    );
+   
+  }
+
+  /*
+    Get a random favorite exercise
+  */
+  String getRandomFavoriteExercise() {
+    List<String> myExercises = ['squats', 'pull up', 'bench'];
+
+    // return random exercise
+    return 'chest';
+  }
+  /* 
+    Get the weight used on first rep from the picked exercise
+  */
+  int firstRepWeight(String myExercises) {
+    // find number of reps from that exercise
+    return 144;
+  }
+  Widget favoriteExercise(BuildContext context) {
+    String name = getRandomFavoriteExercise();
+    int weight = firstRepWeight(name);
+
+    final PageController controller = PageController();
+    return PageView(
+      controller: controller,
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            // Todays Workout
-            Expanded(
-              child:Card( 
-                elevation: 0,
-                color: Theme.of(context).colorScheme.surfaceVariant,
-                child: const SizedBox(
-                  height: 100,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 6),
-                    child: Text('Weight'),
-                  ),
-                ),
-              ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Card(
+            child: ListTile(
+              title: Text(name),
+              subtitle: Text('chart of first rep progress'),
             ),
-            // Weight
-            Expanded(
-              child: Card( 
-                elevation: 0,
-                color: Theme.of(context).colorScheme.surfaceVariant,
-                child: const SizedBox(
-                  height: 100,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 6),
-                    child: Text('card for favorite exercise 2'),
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
-        Row(
-          children: <Widget>[
-            // Todays Workout
-            Expanded(
-              child:Card( 
-                elevation: 0,
-                color: Theme.of(context).colorScheme.surfaceVariant,
-                child: const SizedBox(
-                  height: 100,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 6),
-                    child: Text('favaorite exercise 3'),
-                  ),
-                ),
+            Card(
+              child: ListTile(
+                title: Text('Incline Dumbbell Bench'),
+                subtitle: Text('\t3 x 10'),
               ),
             ),
-            // Weight
-            Expanded(
-              child: Card( 
-                elevation: 0,
-                color: Theme.of(context).colorScheme.surfaceVariant,
-                child: const SizedBox(
-                  height: 100,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 6),
-                    child: Text('card for favorite exercise 4'),
-                  ),
-                ),
+      ],
+    );
+  }
+  
+
+  /*
+    Weight card widget
+  */
+  Widget weightHighlight(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: SizedBox(
+            height: 200,
+            child: Card(
+              child: ListTile(
+                title: Text('Weight Chart'),
+                subtitle: Text(''),
               ),
             ),
-          ],
+          ),
         ),
       ],
     );
   }
+
+/*
+  Widget weightLineChart() {
+    return LineChart(
+      LineChartData(
+
+      )
+    );
+  } 
+  */
+  /*
+    Create a chart for weight card widget
+  */
 }
