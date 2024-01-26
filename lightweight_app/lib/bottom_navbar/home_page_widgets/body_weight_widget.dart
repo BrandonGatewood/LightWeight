a76a8a7d4ffdb64a9ef9acaefa32c0ab8f191e0b
Brandon Gatewood <gatew2@pdx.edu>
2024-01-26 14:29:17 -0800

    added body_weight_widget to track users weight

---
+++ lightweight_app/lib/bottom_navbar/home_page_widgets/body_weight_widget.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class BodyWeight extends StatelessWidget {
+  const BodyWeight({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('hi');
+  }
+} 
